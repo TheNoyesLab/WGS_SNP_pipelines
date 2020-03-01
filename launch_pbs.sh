f77bbf40dff98e0ecfc3e696ae2503375b0dacf6
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2020-03-01 13:19:41 -0700

    Update launch_pbs.sh

--- launch_pbs.sh
+++ launch_pbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -l walltime=8:00:00,mem=5gb,nodes=1:ppn=2
+#PBS -l walltime=96:00:00,mem=5gb,nodes=1:ppn=2
 #PBS -o /scratch.global/test_WGS/outfile_o_file
 #PBS -e /scratch.global/test_WGS/errorfile_e_file
 #PBS -q mesabi
@@ -10,6 +10,6 @@
 module purge
 module load singularity
 
-cd /scratch.global/test_WGS/WGS_SNP_pipelines
+cd /scratch.global/Salmonella_WGS/WGS_SNP_pipelines
 
-/home/noyes046/shared/tools/nextflow run main_combined_pipeline.nf --reference_genome /scratch.global/test_WGS/ref_Ecoli_NC_000913.fasta --reads '/scratch.global/test_WGS/test_genomes/*_{1,2}.fastq' -profile singularity_pbs --output /scratch.global/test_WGS/GenomeTrakr_EcoliShigella_qsub --threads 128 -w /scratch.global/test_WGS/qsub_work_small_ecoli -resume -with-report WGS_tools.report -with-trace -with-timeline
+/home/noyes046/shared/tools/nextflow run main_combined_pipeline.nf --reference_genome /scratch.global/Salmonella_WGS/Senterica_LT2_ref_genome.fasta --reads '/scratch.global/Salmonella_WGS/all_genomes/*_{1,2}.fastq.gz' -profile singularity_pbs --output /scratch.global/Salmonella_WGS/allSources_Salmonella_WGS_results --threads 128 -w /scratch.global/Salmonella_WGS/qsub_work_salmonella -resume -with-report Salmonella_WGS_tools.report -with-trace -with-timeline
