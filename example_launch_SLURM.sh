1aef6ea8e0e9ec8fdc7e5620feddebf6d70ecc45
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2020-12-12 16:07:45 -0700

    Update example_launch_SLURM.sh

--- example_launch_SLURM.sh
+++ example_launch_SLURM.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=AMRPlusPlus
+#SBATCH --job-name=WGS_pipelines
 #SBATCH --partition=cn1107
 #SBATCH --ntasks=1
-#SBATCH --qos=long
 #SBATCH --cpus-per-task=1
 #SBATCH --time=100:00:00
 #SBATCH --export=ALL
@@ -12,7 +11,8 @@
 module load jdk/1.8.0
 module load singularity/2.5.2
 
-mpirun --pernode ./nextflow run main_AmrPlusPlus_v2.nf -resume -profile msi_pbs \
--w /work_dir --threads 15 \
---output output_results --host /PATH/TO/HOST/GENOME \
---reads "RAWREADS/*_R{1,2}.fastq.gz" -with-mpi
+nextflow run main_combined_pipeline.nf --reference_genome /tempalloc/noyes042/FMPRE_clean/Host_genomes/Senterica_LT2_ref_genome.fasta \ 
+--reads '/tempalloc/noyes042/FMPRE_clean/Raw_datasets/100_genome_datasets/100genomes_Salm_HighQuality_IBM/*_{1,2}.fastq.gz' -profile singularity \ 
+--output /tempalloc/noyes042/FMPRE_clean/ALL_results/temp_results/100_Salm_HighQuality_IBM_WGS_results --threads 20 \
+-w /tempalloc/noyes042/FMPRE_clean/ALL_results/temp_results/workSalm_HighQuality_IBM -resume -with-report Salm_HighQuality_IBM_WGS_tools.report \
+-with-trace -with-timeline --species salmonella_enterica
